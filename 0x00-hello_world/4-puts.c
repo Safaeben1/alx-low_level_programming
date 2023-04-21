b054092a067849d177b63306fff4e95e79c16467
Safaeben1 <safae.benajiba29@gmail.com>
2023-04-20 17:10:33 -0700

    je taime

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return: Always 0 (Success)
- */
-
-int main (void)
+	 * main - A program that prints a line whit puts function
+	 * Return: 0 (Success)
+	 */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 } 

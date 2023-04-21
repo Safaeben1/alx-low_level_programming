e80a73475afd816b9feec51ddc816fbd151aba6b
Safaeben1 <safae.benajiba29@gmail.com>
2023-04-21 14:52:55 -0700

    eew

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+* owned by sfiya
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10 ; i++)
+{
+
+printf("%d", i);
+}
+printf("\n");
+
+
+return (0);
+}

5a1dcaf9eeedbe17a04140a91c0217ff6a3d0ce4
Safaeben1 <safae.benajiba29@gmail.com>
2023-04-20 19:13:06 -0700

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,12 @@
  * owned by sfiya
  * Return: Always 0 (Success)
  */
-int main(void) 
+
+int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
-    return 1;
+	char *message = "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
+	return (1);
 }
 

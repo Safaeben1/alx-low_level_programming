24405a5b6fdd0abbbd7f281c25903f072a6fef25
Safaeben1 <safae.benajiba29@gmail.com>
2023-04-25 16:33:58 -0700

    ela slamtk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @n : integer
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}

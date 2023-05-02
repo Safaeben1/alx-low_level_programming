efde58b80e798008df899753a5779b9e3fac9842
Safaeben1 <safae.benajiba29@gmail.com>
2023-05-02 15:07:45 -0700

    boom

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str : string to be printed starting with the first character
+ * owned by sfiya
+ * Return : 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

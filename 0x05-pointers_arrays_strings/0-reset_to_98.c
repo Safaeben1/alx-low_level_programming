235a5694e3ddcf454b165b97acc161da294796f6
Safaeben1 <safae.benajiba29@gmail.com>
2023-05-02 04:17:37 -0700

    boom

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer and updates the value
+ * written by sfiya
+ * @n: int to check
+ * Return: 0 is a success.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

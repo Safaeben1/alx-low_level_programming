29a42b8b34c0006857f4128dcffdf6456bf16722
Safaeben1 <safae.benajiba29@gmail.com>
2023-05-04 15:36:43 -0700

    boom

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
  * *_strcat - concatenates two strings.
  * @dest:char
  * @src:char
- * Return : pointer to the resulting string dest
+ * Return: pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)

cb5a49ba87429bda05fda97aeea2d1fbf26765ec
Safaeben1 <safae.benajiba29@gmail.com>
2023-05-12 06:19:18 -0700

    hihi

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * Owned By sfiya
+ * @argv: argument
+ * @argc: argument count
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n",*argv);
+
+	return (0);
+}

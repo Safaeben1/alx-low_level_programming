b6be48735c1e05273d3cdbccfe0f234120537699
Safaeben1 <safae.benajiba29@gmail.com>
2023-05-24 15:04:58 -0700

    doe

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that writes a name
+ * Owned By Sfiya
+ * @f: a pointer to a function
+ * @name: a pointer to a null-terminated string containing a name
+ */
+
+void	print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+	f(name);
+}

a8107f2301a26ea410c5b5691a6ef0b9d91d08cb
Safaeben1 <safae.benajiba29@gmail.com>
2023-05-25 10:17:59 -0700

    boo

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings
+ * Owned By Sfiya
+ * @separator: String to be printed between strings
+ * @n: number of strings passed
+ * @...: A variable number
+ * Return: 0.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *s;
+	unsigned int i;
+	va_list op;
+
+	va_start(op, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+
+		s = va_arg(op, char*);
+		if (s == NULL)
+		{
+			s = "(nil)";
+		}
+		printf("%s", s);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(op);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - A function with 2 parameters
+ * Owned By Sfiya
+ * @format: specifies the necessary operation
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *st, *s = "";
+	va_list list;
+
+	va_start(list, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", s, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", s, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", s, va_arg(list, double));
+					break;
+				case 's':
+					st = va_arg(list, char *);
+					if (!st)
+						st = "(nil)";
+					printf("%s%s", s, st);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			s = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
